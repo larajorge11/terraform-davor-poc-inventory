fe891727c56bd92e3395b41e6a244c675a849e9e
Jorge Lara <larajorge11@gmail.com>
2021-09-29 14:37:54 -0500

    Update terraform main

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.43.0"
+    }
+  }
+  required_version = "1.0.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
 
 # Module: VPC, Subnets
 module "inventory_vpc" {
