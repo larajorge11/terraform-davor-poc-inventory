c67601e5b879b7cec5cc6d6b77240295cc180e3c
Jorge Lara <larajorge11@gmail.com>
2021-09-30 16:36:27 -0500

    Postgres database

--- rds_module/main.tf
+++ rds_module/main.tf
@@ -1,13 +1,13 @@
-resource "aws_db_subnet_group" "mariadb-subnet" {
-    name = "mariadb-subnet"
+resource "aws_db_subnet_group" "postgresdb-subnet" {
+    name = "postgresdb-subnet"
     description = "RDS subnet group"
     subnet_ids = var.subnet_ids
 }
 
-resource "aws_db_parameter_group" "mariadb-parameters" {
-    name = "mariadb-parameters"
-    family = "mariadb10.5"
-    description = "MariaDB parameter group"
+resource "aws_db_parameter_group" "postgresdb-parameters" {
+    name = "postgresdb-parameters"
+    family = "postgres13.4"
+    description = "Postgres parameter group"
 
     parameter {
         name = "max_allowed_packet"
@@ -15,17 +15,17 @@ resource "aws_db_parameter_group" "mariadb-parameters" {
     }
 }
 
-resource "aws_db_instance" "mariadb" {
+resource "aws_db_instance" "postgres" {
     allocated_storage = 10
-    engine = "mariadb"
-    engine_version = "10.5.12"
+    engine = "postgres"
+    engine_version = "13.4"
     instance_class = "db.t3.small"
-    identifier = "mariadb"
-    name = "mariadb"
+    identifier = "postgres"
+    name = "postgres"
     username = "root"
     password = var.RDS_PASSWORD
-    db_subnet_group_name = aws_db_subnet_group.mariadb-subnet.name
-    parameter_group_name = aws_db_parameter_group.mariadb-parameters.name
+    db_subnet_group_name = aws_db_subnet_group.postgresdb-subnet.name
+    parameter_group_name = aws_db_parameter_group.postgresdb-parameters.name
     multi_az = false
     vpc_security_group_ids = var.vpc_security_group_ids
     storage_type = "gp2"
@@ -33,6 +33,6 @@ resource "aws_db_instance" "mariadb" {
     availability_zone = var.availability_zone
     skip_final_snapshot = true
     tags = {
-        Name = "mariadb-instance"
+        Name = "postgres-instance"
     }
 }
