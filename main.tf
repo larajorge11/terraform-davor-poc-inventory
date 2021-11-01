46e07ea916e2ce9a5c11728635a521d6821089bd
Jorge Lara <larajorge11@gmail.com>
2021-11-01 15:21:41 -0500

    change cidr

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 module "inventory_vpc" {
   source                    = "./vpc_module"
   vpc_id                    = module.inventory_vpc.vpc_id
-  vpc_cidr                  = "10.0.0.0/16"
+  vpc_cidr                  = "0.0.0.0/16"
   subnet_public_cidr        = "10.0.1.0/24"
   subnet_private_cidr       = "10.0.4.0/24"
   availability_zone         = "eu-west-2a"
